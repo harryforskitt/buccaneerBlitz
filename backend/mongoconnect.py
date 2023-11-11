4e0eb5ddd6cbc754e445fcf3ee805c6ceac1d51e
harryforskitt <harry.forskitt@icloud.com>
2023-11-11 16:26:04 +0000

    tested connection to CosmosDB MongoDB

---
+++ backend/mongoconnect.py
@@ -0,0 +1,44 @@
+#test connection to mongodb
+
+#When moving to production, will probably need to fix SSL cert issue with this: https://www.youtube.com/watch?v=dEBN1M609zk
+
+import os
+import sys
+
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+CONNECTION_STRING = os.environ.get("COSMOS_CONNECTION_STRING")
+client = pymongo.MongoClient(CONNECTION_STRING)
+
+mydb = client['society']
+mycol = mydb['users']
+
+#insert
+new_user = {"_id": "0", "username": "harry", "password": "password"}
+
+mycol.insert_one(new_user)
+
+#read
+for i in mycol.find():
+    print(i)
+
+# for prop, value in vars(client.options).items():
+#     print("Property: {}: Value: {} ".format(prop, value))
+
+# # Get server information
+# print("Server info:")
+# for k, v in client.server_info().items():
+#     print("Key: {} , Value: {}".format(k, v))
+
+# # Get server status of admin database
+# print("Server status:")
+# print("Server status {}".format(client.admin.command("serverStatus")))
+
+# # List databases
+# print("Databases:")
+# databases = client.list_database_names()
+# print("Databases: {}".format(databases))
+
+client.close()
